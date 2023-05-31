30a5d7224ed39ce6d793fd2a12067315a1f423af
MisterConscio <saulo.costam@gmail.com>
2023-05-31 12:14:31 -0300

    Adicionado sistema redux ao carrinho

---
+++ src/store/reducers/favorito.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type FavoritoState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritoState = {
+  itens: []
+}
+
+const favoritoSlice = createSlice({
+  name: 'favorito',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+      //const favoritos = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        state.itens = state.itens.filter((p) => p.id !== produto.id)
+        //state.itens.push(favoritosSemProduto)
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritoSlice.actions
+export default favoritoSlice.reducer
+
+// function favoritar(produto: Produto) {
+//   if (favoritos.find((p) => p.id === produto.id)) {
+//     const favoritosSemProduto = favoritos.filter((p) => p.id !== produto.id)
+//     setFavoritos(favoritosSemProduto)
+//   } else {
+//     setFavoritos([...favoritos, produto])
+//   }
+// }
+
